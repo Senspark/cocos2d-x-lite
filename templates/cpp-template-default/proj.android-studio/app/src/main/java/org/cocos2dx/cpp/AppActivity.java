d4bd7640d54e9c329b168e8125e30e720895ebc1
enrevol <enrevol@gmail.com>
2017-05-29 17:20:10 +0700

    Organize android project.

--- templates/cpp-template-default/proj.android-studio/app/src/main/java/org/cocos2dx/cpp/AppActivity.java
+++ templates/cpp-template-default/proj.android-studio/app/src/main/java/org/cocos2dx/cpp/AppActivity.java
@@ -23,7 +23,7 @@ THE SOFTWARE.
 ****************************************************************************/
 package org.cocos2dx.cpp;
 
-import org.cocos2dx.lib.Cocos2dxActivity;
+import android.app.Activity;
 
-public class AppActivity extends Cocos2dxActivity {
+public class AppActivity extends Activity {
 }
