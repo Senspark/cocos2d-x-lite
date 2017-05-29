0b12b26aedf6cda682f900d9faae8389646d6c1a
enrevol <enrevol@gmail.com>
2017-05-29 18:19:02 +0700

    Update project mk.

---
+++ templates/cpp-template-default/proj.android-studio/app/src/main/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+PROJECT_DIR := $(LOCAL_PATH)/../../../../..
+
+$(call import-add-path, $(PROJECT_DIR)/cocos2d)
+
+LOCAL_MODULE := cocos2dcpp
+
+LOCAL_SRC_FILES := cpp/main.cpp
+LOCAL_SRC_FILES += ${shell find $(PROJECT_DIR)/Classes -name "*.cpp" -print}
+
+LOCAL_C_INCLUDES := ${shell find $(PROJECT_DIR)/Classes -type d -print}
+
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module, cocos)
